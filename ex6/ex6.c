532bc1266ca748a0de4fcc4e6e13b646ed2ae81f
kjohna <43828763+kjohna@users.noreply.github.com>
2019-05-30 15:28:25 -0600

    ex6 sorta working with my own types

--- ex6/ex6.c
+++ ex6/ex6.c
@@ -15,12 +15,46 @@ and `clock_gettime()` should work just fine.
 #include <unistd.h>
 #include <time.h>
 
-#define number_iter 1000000
+#define number_iter 50 //1000000
 #define BILLION 1000000000L
 
 int main()
 {
     // Your code here
-    
+    // a struct for clock_gettime() to populate:
+    struct timespec start, end;
+    // store difference calc here
+    double diff, avg;
+    int first = 1;
+
+    for (int i = 0; i < number_iter; i++)
+    {
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        // time how long this takes:
+        printf("");
+        // sleep(1);
+        clock_gettime(CLOCK_MONOTONIC, &end);
+        diff = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000000000.;
+
+        // printf("start: %lf\n", start.tv_sec + start.tv_nsec / 1000000000.);
+        // printf("end: %lf\n", end.tv_sec + end.tv_nsec / 1000000000.);
+        // printf("%lf\n", diff);
+        // calc simple moving average:
+        if (first)
+        {
+            avg = diff;
+            first = 0;
+        }
+        else
+        {
+            avg = (diff + (float)i * avg) / ((float)i + 1);
+        }
+        // if (!(i % 100000))
+        // {
+        printf("current: %lf ", diff);
+        printf("%d, average time taken: %lf s\n", i, avg);
+        // }
+    }
+    printf("FINAL average time taken: %lf s after %d runs.\n", avg, number_iter);
     return 0;
 }
